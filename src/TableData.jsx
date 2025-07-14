b4e283b1b1f50581345280ec41506cc9f159c9db
SOURAJRAI <sourajch84@gmail.com>
2025-07-14 16:20:03 +0530

    Updated

--- src/TableData.jsx
+++ src/TableData.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FaLongArrowAltDown, FaLongArrowAltUp, FaTrash } from "react-icons/fa";
 import { IoCloseOutline } from "react-icons/io5";
 
